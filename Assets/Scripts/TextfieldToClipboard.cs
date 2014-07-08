38b8561e6bce01780814a8e8e09abe34fa398053
SnowKrown <christian.montenegro@brainz.co>
2014-07-08 12:49:47 -0500

    Added simple GUI for test

---
+++ Assets/Scripts/TextfieldToClipboard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TextfieldToClipboard : MonoBehaviour 
+{
+	[SerializeField]
+	private float width = 300;
+
+	private string myText = "";
+
+	private void OnGUI ()
+	{
+		DrawWindow ();
+	}
+
+	private void DrawWindow ()
+	{
+		myText = GUILayout.TextField (myText, GUILayout.MinWidth (width));
+
+		if (GUILayout.Button ("Copy to Clipboard", GUILayout.MinWidth (width)))
+			UniClipboard.value = myText;
+	}
+}
