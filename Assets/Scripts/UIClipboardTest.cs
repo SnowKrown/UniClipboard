59adc51e18b2d54e96d4af20a005dff39869f055
olgasan <oh.cardenas@gmail.com>
2014-07-08 16:04:08 -0500

    Added test scene.

---
+++ Assets/Scripts/UIClipboardTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIClipboardTest : MonoBehaviour 
+{	
+	private string text = "";
+
+	private void OnGUI () 
+	{
+		text = GUILayout.TextField (text, GUILayout.MinWidth (500), GUILayout.MinHeight (100));
+
+		if (GUILayout.Button ("Save to clipboard", GUILayout.MinWidth (120), GUILayout.MinHeight (60)))
+			Clipboard.value = text;
+	}
+}
