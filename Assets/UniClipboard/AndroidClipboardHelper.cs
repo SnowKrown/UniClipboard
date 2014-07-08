01174fa6014762c66eb95d9c7b19a491cc44a85b
SnowKrown <christian.montenegro@brainz.co>
2014-07-08 14:30:53 -0500

    no message

---
+++ Assets/UniClipboard/AndroidClipboardHelper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace UniClipboardHelper
+{
+#if UNITY_ANDROID
+	public static class Android
+	{
+		[DllImport("__Internal")]
+		private static extern void _SetClipboard(string value);
+		
+		[DllImport("__Internal")]
+		private static extern string _GetClipboard();
+
+		public static void SetClipboard (string value)
+		{
+			_SetClipboard (value);
+		}
+
+		public static string GetClipboard ()
+		{
+			return _GetClipboard ();
+		}
+	}
+#endif
+}
